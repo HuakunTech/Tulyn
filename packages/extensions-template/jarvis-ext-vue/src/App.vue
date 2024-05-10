f1329411b8590a0922a31649d2d6fba03da0d3a3
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-10 03:57:15 -0400

    Add shadcn to vue extension template

--- packages/extensions-template/jarvis-ext-vue/src/App.vue
+++ packages/extensions-template/jarvis-ext-vue/src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="border h-screen flex flex-col justify-center">
+    <div class="flex justify-center">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vue + Jarvis Extension Template" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
