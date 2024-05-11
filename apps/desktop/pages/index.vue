abf6333f763aaa2fcc8b4c19d4a3b618dd3d52a3
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-11 08:56:46 -0400

    Add astro extension template

--- apps/desktop/pages/index.vue
+++ apps/desktop/pages/index.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { WebviewWindow } from "@tauri-apps/api/window";
-
-function openExt() {
-  new WebviewWindow("plugin-b", {
-    url: "http://localhost:1566/extensions/myip/dist/",
-  });
-}
+import {
+  startServer,
+  stopServer,
+  serverIsRunning,
+} from "@/lib/commands/server";
 </script>
 <template>
   <NuxtLayout>
     <main class="px-5">
-      <Button @click="openExt()">Click me</Button>
+      <Button @click="startServer">Start Server</Button>
       <ModeToggle />
       <Button>
         <NuxtLink to="/extensions">Extensions</NuxtLink>
