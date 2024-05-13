3f10d6707aa16b1c99bee5a476f26fc66ac62902
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-12 22:53:31 -0400

    Implement UI for developer tab

--- apps/desktop/components/settings/DeveloperTab.vue
+++ apps/desktop/components/settings/DeveloperTab.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
+const tags = Array.from({ length: 50 }).map((_, i, a) => `v1.2.0-beta.${a.length - i}`);
 </script>
+
 <template>
-    
+  <div class="py-2 max-h-full overflow-auto flex">
+    <ScrollArea class="max-h-full w-48 rounded-md border">
+      <div class="p-4">
+        <h4 class="mb-4 text-sm font-medium leading-none">Tags</h4>
+
+        <div v-for="tag in tags" :key="tag">
+          <div class="text-sm">
+            {{ tag }}
+          </div>
+          <Separator class="my-2" />
+        </div>
+      </div>
+    </ScrollArea>
+    <SettingsDeveloperInstallation class="grow" />
+  </div>
 </template>
--- apps/desktop/pages/settings.vue
+++ apps/desktop/pages/settings.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {
-  CogIcon,
-  InfoIcon,
-  UserRoundIcon,
-  SquareTerminalIcon,
-} from "lucide-vue-next";
+import { CogIcon, InfoIcon, UserRoundIcon, SquareTerminalIcon } from "lucide-vue-next";
 import { Icon } from "@iconify/vue";
 </script>
 
 <template>
   <NuxtLayout>
     <Tabs default-value="general" class="w-full h-full flex flex-col px-4">
-      <TabsList class="grid w-full grid-cols-5">
+      <TabsList class="grid w-full grid-cols-5 select-none">
         <TabsTrigger value="general">
           <CogIcon class="mr-2 h-5 w-5" />
           General
         </TabsTrigger>
         <TabsTrigger value="extensions">
-          <Icon
-            icon="fluent:developer-board-lightning-20-regular"
-            class="mr-2 h-5 w-5"
-          />
+          <Icon icon="fluent:developer-board-lightning-20-regular" class="mr-2 h-5 w-5" />
           Extensions
         </TabsTrigger>
         <TabsTrigger value="account">
@@ -39,7 +31,11 @@ import { Icon } from "@iconify/vue";
       </TabsList>
       <TabsContent value="general" class="grow"><SettingsGeneralTab /></TabsContent>
       <TabsContent value="extensions"><SettingsExtensionsTab /></TabsContent>
-      <TabsContent value="developer"><SettingsDeveloperTab /></TabsContent>
+      <TabsContent value="developer" class="grow min-h-0">
+        <!-- <div class="overflow-auto flex"> -->
+          <SettingsDeveloperTab />
+        <!-- </div> -->
+      </TabsContent>
       <TabsContent value="account"></TabsContent>
       <TabsContent value="about" class="grow">
         <SettingsAboutTab class="" />
