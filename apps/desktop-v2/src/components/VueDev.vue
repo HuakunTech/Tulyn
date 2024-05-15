107ad55853168b03e51892a56a4fe3fdefae2aa6
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-14 20:48:03 -0400

    Add Tauri desktop v2 starter code

---
+++ apps/desktop-v2/src/components/VueDev.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { Window } from "@tauri-apps/api/window";
+import { Webview } from "@tauri-apps/api/webview";
+
+function loadWebView() {
+  const appWindow = new Window("astro");
+  console.log("window");
+  const webview = new Webview(appWindow, "astro-label", {
+    url: "/about",
+    // url: 'https://github.com/tauri-apps/tauri',
+  });
+}
+</script>
+<template>
+  <Button @click="loadWebView">Load WebView</Button>
+</template>
