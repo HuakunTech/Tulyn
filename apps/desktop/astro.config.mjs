2648166e5a8535161f0b619b203601a1b19ee7b4
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-16 23:16:56 -0400

    Fix CI

--- apps/desktop/astro.config.mjs
+++ apps/desktop/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
@@ -6,5 +6,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind(), react()]
+  integrations: [vue(), tailwind(), react()],
 });
