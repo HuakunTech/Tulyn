99269c22e850207cd6ab1deaa85a66cedad6f929
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-20 08:01:44 -0400

    Remove 2 rust files from bin folder, which prevent tauri app from building

--- apps/desktop/src-tauri/src/bin/debug.rs
+++
@@ -1,7 +0,0 @@
-use std::path::PathBuf;
-
-fn main() {
-    let path = PathBuf::from("/Applications/Visual Studio Code.app/Contents/Resources/Code.icns");
-    let ext = path.extension().unwrap().to_str().unwrap();
-    println!("path: {:?}", ext);
-}
