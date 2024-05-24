02f3c6aed29590988ea1fe616dc24406b25e46a1
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-23 20:10:08 -0400

    Update docs, use not-content to fix editor display

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Jarvis",
-      customCss: ["./src/tailwind.css", "./src/monaco-editor-fix.css"],
+      customCss: ["./src/tailwind.css"],
       logo: {
         src: "./src/assets/logo.png",
       },
