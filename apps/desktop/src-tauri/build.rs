a582ce93a90d79927c61c588d6f71c7b39237a7d
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-25 17:36:34 -0400

    Remove applications-rs from vendors.

--- apps/desktop/src-tauri/build.rs
+++ apps/desktop/src-tauri/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    // let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     // tonic_build::configure()
     //     .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
     //     .compile(&["proto/helloworld.proto"], &["proto"])
