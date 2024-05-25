d042c81b9bc351466e89d789ceaeff10ff749f9a
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-25 17:40:57 -0400

    Remove desktop2

--- apps/desktop2/src-tauri/src/commands/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod apps;
-pub mod dev;
-pub mod extension;
-pub mod fs;
-pub mod server;
-pub mod system;
-pub mod utils;
--- apps/desktop2/src-tauri/src/utils/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod fs;
-pub mod icns;
-pub mod manifest;
-pub mod path;
-pub mod settings;
-pub mod setup;
