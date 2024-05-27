016ae78bd3602d5f0c61d302da18270850ea9b5d
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-27 02:59:35 -0400

    Add supabase graphql API codegen library

---
+++ packages/gql/codegen.ts
@@ -0,0 +1,41 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import { addTypenameSelectionDocumentTransform } from "@graphql-codegen/client-preset";
+import { z } from "zod";
+
+const endpoint = z.string().parse(process.env.SUPABASE_GRAPHQL_ENDPOINT);
+const schema: any = {};
+schema[endpoint] = {
+  headers: {
+    apiKey: z.string().parse(process.env.SUPABASE_ANON_KEY),
+  },
+};
+const config: CodegenConfig = {
+  schema: schema,
+  documents: "src/**/*.graphql",
+  overwrite: true,
+  ignoreNoDocuments: true,
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      documentTransforms: [addTypenameSelectionDocumentTransform],
+      plugins: [],
+      config: {
+        scalars: {
+          UUID: "string",
+          Date: "string",
+          Time: "string",
+          Datetime: "string",
+          JSON: "string",
+          BigInt: "string",
+          BigFloat: "string",
+          Opaque: "any",
+        },
+      },
+    },
+  },
+  hooks: {
+    // afterAllFileWrite: ["npm run prettier"], // optional
+  },
+};
+
+export default config;
