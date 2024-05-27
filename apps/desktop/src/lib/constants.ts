5b6378de1852e3b028ae30b3a5482735973a857e
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-27 00:05:52 -0400

    Minor update

--- apps/desktop/src/lib/constants.ts
+++ apps/desktop/src/lib/constants.ts
@@ -1,5 +1,7 @@
+import { z } from "zod";
 import { appDataDir, join } from "@tauri-apps/api/path";
 
 const appDataDirPath = await appDataDir();
 export const extensionsFolder = await join(appDataDirPath, "extensions");
-
+export const SUPABASE_URL = z.string().min(10).parse(process.env.SUPABASE_URL);
+export const SUPABASE_ANON_KEY = z.string().min(10).parse(process.env.SUPABASE_ANON_KEY);
