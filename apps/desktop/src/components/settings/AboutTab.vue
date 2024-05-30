ad1da5b9712e7badaaf62861b7ec1da9d2b056da
Huakun Shen <huakun.shen@huakunshen.com>
2024-05-30 09:21:06 -0400

    Add manual update check feature.

--- apps/desktop/src/components/settings/AboutTab.vue
+++ apps/desktop/src/components/settings/AboutTab.vue
@@ -5,6 +5,10 @@ import { getVersion } from "@tauri-apps/api/app";
 import { onMounted, ref, type HTMLAttributes } from "vue";
 import { Icon } from "@iconify/vue";
 import { cn } from "@/lib/utils";
+import { check } from "@tauri-apps/plugin-updater";
+import { relaunch } from "@tauri-apps/plugin-process";
+import { Button } from "@/components/ui/button";
+import { confirm } from "@tauri-apps/plugin-dialog";
 
 const appVersion = ref("");
 onMounted(() => {
@@ -15,6 +19,19 @@ onMounted(() => {
 const props = defineProps<{
   class?: HTMLAttributes["class"];
 }>();
+
+async function checkUpdate() {
+  const update = await check();
+  if (update?.available) {
+    const confirmUpdate = await confirm(
+      `A new version ${update.version} is available. Do you want to install and relaunch?`,
+    );
+    if (confirmUpdate) {
+      await update.downloadAndInstall();
+      await relaunch();
+    }
+  }
+}
 </script>
 <template>
   <Card
@@ -23,7 +40,7 @@ const props = defineProps<{
     "
   >
     <CardContent class="flex space-x-5 items-center">
-      <img src="/128x128.png" class=" w-44" />
+      <img src="/128x128.png" class="w-44" />
       <div class="flex flex-col space-y-1">
         <p class="text-3xl font-bold">Jarvis</p>
         <p class="text-xs">Version: {{ appVersion }}</p>
@@ -42,6 +59,7 @@ const props = defineProps<{
           Extensions Source Code
           <Icon icon="mdi:github" class="inline text-white -translate-y-0.5" />
         </TauriLink>
+        <Button @click="checkUpdate" size="xs" variant="secondary">Check Update</Button>
       </div>
     </CardContent>
   </Card>
