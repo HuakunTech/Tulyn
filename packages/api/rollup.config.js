299e4cccef5669ff5e52c56c572af2df6bc6a543
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-01 00:16:45 -0400

    Build jarvis-api to js package.

---
+++ packages/api/rollup.config.js
@@ -0,0 +1,49 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { cwd } from "process";
+import typescript from "@rollup/plugin-typescript";
+
+const pkg = JSON.parse(readFileSync(join(cwd(), "package.json"), "utf8"));
+
+export default [
+  {
+    input: "index.ts",
+    output: [
+      {
+        file: "./dist/index.js",
+        format: "esm",
+      },
+    ],
+    plugins: [
+      typescript({
+        declaration: true,
+        declarationDir: `./.`,
+      }),
+    ],
+    external: [
+      /^@tauri-apps\/api/,
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+  },
+  {
+    input: "ui/index.ts",
+    output: [
+      {
+        file: "./dist/ui.js",
+        format: "esm",
+      },
+    ],
+    plugins: [
+      typescript({
+        declaration: true,
+        declarationDir: `./.`,
+      }),
+    ],
+    external: [
+      /^@tauri-apps\/api/,
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+  },
+];
