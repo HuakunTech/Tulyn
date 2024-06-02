9484bf55dd22fb66651edb6b84cf55edca663e2e
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-01 22:38:57 -0400

    Add shell capability

--- apps/desktop/src/components/Dev.vue
+++ apps/desktop/src/components/Dev.vue
@@ -7,7 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 import { tauriToast } from "./tauri/toast";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { ElMessage } from "element-plus";
-
+import { Command } from "@tauri-apps/plugin-shell";
 import "element-plus/dist/index.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
 
@@ -32,6 +32,34 @@ async function trigger() {
   //   alwaysOnTop: true,
   // });
 }
+
+onMounted(async () => {
+  // const output = await Command.create("exec-echo", ["message"]).execute();
+  // console.log(output);
+  // console.log(output.stdout);
+
+  // assert(output.code === 0);
+  // assert(output.signal === null);
+  // assert(output.stdout === "message");
+  // assert(output.stderr === "");
+  // const command = Command.create("exec-echo", ["hello"]);
+  const command = Command.create("exec-ffprobe", ["/Users/hacker/Desktop/kxzp.mp4"]);
+  // const out = await command.execute()
+  // console.log(out.stdout);
+  
+  // const command = Command.create("exec-bash", ["-c", "echo hello world", ""]);
+  // const command = Command.create("exec-bash", ["-c", "ps aux"]);
+  
+  command.on("close", (data) => {
+    console.log(`command finished with code ${data.code} and signal ${data.signal}`);
+  });
+  command.on("error", (error) => console.error(`command error: "${error}"`));
+  command.stdout.on("data", (line) => console.log(`command stdout: "${line}"`));
+  command.stderr.on("data", (line) => console.log(`command stderr: "${line}"`));
+
+  const child = await command.spawn();
+  console.log("pid:", child.pid);
+});
 
 function openWindow() {
   // new WebviewWindow('ext', {
