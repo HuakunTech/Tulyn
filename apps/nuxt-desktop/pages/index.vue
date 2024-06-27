54a8e8cae9fb0572df82a861f8a7761a6bfda446
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-27 07:07:07 -0400

    Add shadcn for nuxt

--- apps/nuxt-desktop/pages/index.vue
+++ apps/nuxt-desktop/pages/index.vue
@@ -4,8 +4,11 @@ import clipboard from 'tauri-plugin-clipboard-api';
 onMounted(() => {
     clipboard.readText().then(console.log)
 })
+const { x, y } = useMouse()
 </script>
 <template>
     <h1>Hello World</h1>
-
+    <Button>Hello</Button>
+    <ModeToggle />
+    <div>pos: {{ x }}, {{ y }}</div>
 </template>
