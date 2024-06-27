77e220ac0d9a8411deb27af85f4565a14a2e5d93
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-27 06:12:04 -0400

    init nuxt-desktop with some plugins

--- apps/nuxt-desktop/src-tauri/src/main.rs
+++ apps/nuxt-desktop/src-tauri/src/main.rs
@@ -8,8 +8,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let shell_unlocked = true;
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_shellx::init(shell_unlocked))
+        // .plugin(tauri_plugin_jarvis::init())
+        // .plugin(tauri_plugin_clipboard::init())
+        // .plugin(tauri_plugin_network::init())
+        // .plugin(tauri_plugin_system_info::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
