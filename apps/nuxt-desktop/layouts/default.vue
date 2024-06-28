b97f72313fcb6523e230aed0bf5a778a287fdf11
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-28 06:27:10 -0400

    Fixed sonner

--- apps/nuxt-desktop/layouts/default.vue
+++ apps/nuxt-desktop/layouts/default.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
-import { Toaster } from "@/components/ui/sonner";
+<!-- <script setup lang="ts">
+// import { Toaster } from "@/components/ui/sonner";
+import { Toaster, toast } from 'vue-sonner'
 
 const colorMode = useColorMode();
 </script>
 <template>
   <Toaster />
   <slot />
+</template> -->
+
+<template>
+  <Toaster />
+  <button @click="() => toast('My first toast')">Give me a toast</button>
+  <slot />
 </template>
+
+<script lang="ts" setup>
+import {
+  // Toaster,
+  toast,
+} from "vue-sonner";
+import { Toaster } from "@/components/ui/sonner";
+</script>
--- apps/nuxt-desktop/pages/index.vue
+++ apps/nuxt-desktop/pages/index.vue
@@ -8,7 +8,4 @@ onMounted(() => {
 });
 </script>
 <template>
-  <el-button @click="ElMessage('hello')">button</el-button>
-  <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
-  <LazyElButton type="warning">lazy button</LazyElButton>
 </template>
