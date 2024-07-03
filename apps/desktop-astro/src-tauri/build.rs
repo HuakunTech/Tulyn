22df7a0f3b02801584e2d161777fc6e72ea9cfa8
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-03 09:54:17 -0400

    Remove desktop-astro

--- apps/desktop-astro/src-tauri/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    // tonic_build::configure()
-    //     .file_descriptor_set_path(out_dir.join("helloworld_descriptor.bin"))
-    //     .compile(&["proto/helloworld.proto"], &["proto"])
-    //     .expect("Failed to compile protos");
-    tauri_build::build();
-    // tonic_build::compile_protos("proto/helloworld.proto").expect("Failed to compile protos");
-}
--- apps/desktop-astro/src/components/settings/General/TriggerHotkey.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="grid grid-cols-2 gap-4">
-    <span class="justify-self-end">Trigger Hotkey</span>
-    <div>Pick Hotkey</div>
-  </div>
-</template>
