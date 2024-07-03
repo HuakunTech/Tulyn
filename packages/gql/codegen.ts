692e71e037ae88a6d05d500ad8a8d45ea57fffd9
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-03 08:28:28 -0400

    Fix CI

--- packages/gql/codegen.ts
+++ packages/gql/codegen.ts
@@ -2,6 +2,13 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import { addTypenameSelectionDocumentTransform } from "@graphql-codegen/client-preset";
 import { z } from "zod";
 
+if (!process.env.SUPABASE_GRAPHQL_ENDPOINT) {
+  console.error("SUPABASE_GRAPHQL_ENDPOINT is not set");
+}
+if (!process.env.SUPABASE_ANON_KEY) {
+  console.error("SUPABASE_ANON_KEY is not set");
+}
+
 const endpoint = z
   .string()
   .describe("SUPABASE_GRAPHQL_ENDPOINT Env Var")
