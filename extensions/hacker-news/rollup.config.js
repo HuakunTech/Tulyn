7a66d8babf58aaeb03861679502838b7693a1a5f
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-14 00:43:39 -0400

    feat: define list view schema with valibot

--- extensions/hacker-news/rollup.config.js
+++ extensions/hacker-news/rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs"
 import resolve from "@rollup/plugin-node-resolve"
 import terser from "@rollup/plugin-terser"
-import typescript from "@rollup/plugin-typescript"
+import typescript from "rollup-plugin-typescript2"
 import { visualizer } from "rollup-plugin-visualizer"
 
 /** @type {import('rollup').RollupOptions} */
---
+++ packages/worker-components/index.ts
@@ -0,0 +1,14 @@
+import {
+  array,
+  boolean,
+  enum_,
+  literal,
+  nullable,
+  object,
+  optional,
+  string,
+  union,
+  type InferOutput
+} from "valibot"
+
+export * as List from "./src/list"
