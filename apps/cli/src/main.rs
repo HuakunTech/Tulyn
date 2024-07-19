577959ce824b4e50143db3146a10c2b223346c42
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-19 15:28:09 -0400

    🔥 Remove rust and go cli

--- apps/cli/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let log_file = format!(
-        "jarvis-{}.log",
-        chrono::Local::now().format("%Y-%m-%d-%H-%M-%S")
-    );
-    println!("{}", chrono::Local::now().format("%Y-%m-%d-%H-%M-%S"));
-}
