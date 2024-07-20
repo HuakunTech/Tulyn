d140f39b12b4bc5ac4b9197c353f5fb7a547a6cd
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-20 00:57:03 -0400

    🚚 Rename create-jarvis to create-tulyn

--- apps/create-tulyn/scripts/clean-dist.ts
+++ apps/create-tulyn/scripts/clean-dist.ts
@@ -1,10 +1,11 @@
 import path from "path"
 import fs from "fs-extra"
 import { rootDir } from "../src/constants"
-import { cleanWorkerExtension } from "../src/patch"
+import { cleanExtension } from "../src/patch"
 
 const distPath = path.join(rootDir, "dist")
 /* -------------------------------------------------------------------------- */
 /*                              Worker Extension                              */
 /* -------------------------------------------------------------------------- */
-cleanWorkerExtension(path.join(distPath, "templates", "template-ext-worker"))
+cleanExtension(path.join(distPath, "templates", "template-ext-worker"))
+cleanExtension(path.join(distPath, "templates", "template-ext-react"))
