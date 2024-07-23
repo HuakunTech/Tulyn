245f00bae19369c55a5efc1524f519ae18cf3b53
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-22 20:51:15 -0400

    ✨ Add dance video json deserialize/decompress library

---
+++ packages/json-serde/__tests__/index.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test"
+import { decompressFrame, deserializeFrame } from "../index"
+
+test("Test Deserialize", () => {
+  const frame_str = "ooolloo\nllolloo"
+  const frame = [
+    [0, 0, 0, 1, 1, 0, 0],
+    [1, 1, 0, 1, 1, 0, 0]
+  ]
+  expect(deserializeFrame(frame_str)).toEqual(frame)
+})
+
+test("Test Decompress", () => {
+  const compressed_frame = "3o2l2o\n2lo2l2o"
+  const decompressed_frame = "ooolloo\nllolloo"
+  expect(decompressFrame(compressed_frame)).toBe(decompressed_frame)
+})
