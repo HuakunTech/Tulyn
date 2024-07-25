8358f41748e1bd191013e37213ce55710d999256
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-24 22:31:10 -0400

    ✨ Add theme config to react component lib

--- packages/component-lib/react/src/App.tsx
+++ packages/component-lib/react/src/App.tsx
@@ -1,16 +1,22 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import { CommandDemo } from "@/components/cmd/demo"
-import { ModeToggle } from "@/components/theme-toggle"
+import { ThemeProvider } from "@/components/theme"
+import { ThemeCustomizer } from "@/components/theme/theme-customizer"
+import { Button } from "@/components/ui/button"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import "@/registry/themes.css"
+import { ThemeWrapper } from "@/components/theme/theme-wrapper"
+import React from "react"
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="w-full flex-col h-screen flex justify-center items-center space-y-5">
-        <ModeToggle />
-        <div className="w-[35em] h-[25em]">
-          <CommandDemo />
-        </div>
-      </div>
+      <ThemeWrapper className="relative flex flex-col items-start md:flex-row md:items-center">
+        <TooltipProvider>
+          <ThemeCustomizer />
+        </TooltipProvider>
+      </ThemeWrapper>
+      <ThemeWrapper>
+        <Button>Hello</Button>
+      </ThemeWrapper>
     </ThemeProvider>
   )
 }
