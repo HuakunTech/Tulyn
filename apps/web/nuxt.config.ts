83076e305f7f488c454882757d7fa0650148e76a
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-25 04:53:12 -0400

    🎨 Got nuxt-base integrated into web package

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  extends: ["../../packages/nuxt-base"]
+
+  // components: true
+  extends: ["../../packages/nuxt-base"],
+
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui"
+  }
 })
