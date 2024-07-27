f9970fe834015cd804f85076576439185305743a
Huakun Shen <huakun.shen@huakunshen.com>
2024-07-27 01:25:53 -0400

    🐛 Fix: circular dependency

--- packages/api/src/ui/api/worker-ui.ts
+++ packages/api/src/ui/api/worker-ui.ts
@@ -1,8 +1,6 @@
 import { getDefaultClientAPI } from "tauri-api-adapter"
 import { type IUiWorker } from "../client"
 import type { IUiWorkerServer } from "../server"
-import type { IComponent } from "../worker"
-import type { List } from "../worker/schema/list"
 
 const defaultClientAPI = getDefaultClientAPI<IUiWorkerServer>()
 // export const comlinkUI: IUiWorker = getDefaultClientAPI<IUiWorker>()
