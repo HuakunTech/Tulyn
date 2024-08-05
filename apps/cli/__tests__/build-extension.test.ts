51966b0f901773f44438683d903c38b1c38fcb6f
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-05 00:43:02 -0400

    test: update CLI build extension test to test building all templates with docker

--- apps/cli/__tests__/build-extension.test.ts
+++ apps/cli/__tests__/build-extension.test.ts
@@ -1,6 +1,7 @@
 import os from "os"
 import path from "path"
 import { getRootDir } from "@/constants"
+import type { BuildResult } from "@/types"
 import { buildWithDocker, buildWithDockerAndValidate } from "@/utils"
 import { $ } from "bun"
 import { afterAll, beforeAll, describe, expect, test } from "bun:test"
@@ -8,25 +9,48 @@ import fs from "fs-extra"
 
 const rootDir = getRootDir()
 const createKKDir = path.join(rootDir, "../create-kunkun")
+// const extensionsDir = path.join(rootDir, "../extensions")
+// const extensionNames = fs.readdirSync(extensionsDir)
+// const extensionsDirs = extensionNames
+// 	.map((name) => path.join(extensionsDir, name))
+// 	.filter((p) => fs.existsSync(path.join(p, "package.json")))
+
 const createKKDistDir = path.join(createKKDir, "dist")
 const createKKIndexjsPath = path.join(createKKDistDir, "index.mjs")
 const testDir = path.join(os.tmpdir(), "kunkun-cli-test")
 console.log("Test Dir: ", testDir)
+const templateNames = ["template", "react", "vue", "nuxt", "svelte", "sveltekit"]
 
 fs.rmdirSync(testDir, { recursive: true })
 fs.mkdirpSync(testDir)
 
-await $`node ${createKKIndexjsPath} --outdir ${testDir} --name svelte-ext --template svelte`
-const templateDir = path.join(testDir, "svelte-ext")
-expect(fs.existsSync(templateDir)).toBeTrue()
-const buildResult = await buildWithDockerAndValidate(templateDir)
-console.log(buildResult)
+const templateData: Record<string, { dir: string; buildResult: BuildResult }> = {}
+// const extensionData: Record<string, { dir: string; buildResult: BuildResult }> = {}
 
-test("Build extension with docker", async () => {
-	const expectedTarballPath = path.join(templateDir, buildResult.tarballFilename)
+await Promise.all(
+	templateNames.map(async (templateName) => {
+		const folderName = `${templateName}-ext`
+		await $`node ${createKKIndexjsPath} --outdir ${testDir} --name ${folderName} --template ${templateName}`
+		const templateDir = path.join(testDir, folderName)
+		const buildResult = await buildWithDockerAndValidate(templateDir)
+		templateData[templateName] = {
+			dir: templateDir,
+			buildResult
+		}
+	})
+)
 
-	expect(buildResult).toBeTruthy()
-	expect(await Bun.file(expectedTarballPath).exists()).toBeTrue()
+test("Template Exist", () => {
+	Object.entries(templateData).forEach(async ([templateName, { dir }]) => {
+		expect(await Bun.file(dir).exists()).toBeTrue()
+	})
+})
+
+test("Build Result Tarball Exist", () => {
+	Object.entries(templateData).forEach(async ([templateName, { buildResult, dir }]) => {
+		const expectedTarballPath = path.join(dir, buildResult.tarballFilename)
+		expect(await Bun.file(expectedTarballPath).exists()).toBeTrue()
+	})
 })
 
 afterAll(() => {
