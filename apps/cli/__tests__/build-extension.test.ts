e47a5ca9b4c8e350342137b0c3e8d3b0d71b084f
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-05 00:58:06 -0400

    test: fix CLI build ext test

--- apps/cli/__tests__/build-extension.test.ts
+++ apps/cli/__tests__/build-extension.test.ts
@@ -2,18 +2,13 @@ import os from "os"
 import path from "path"
 import { getRootDir } from "@/constants"
 import type { BuildResult } from "@/types"
-import { buildWithDocker, buildWithDockerAndValidate } from "@/utils"
+import { buildWithDockerAndValidate } from "@/utils"
 import { $ } from "bun"
-import { afterAll, beforeAll, describe, expect, test } from "bun:test"
+import { afterAll, expect, test } from "bun:test"
 import fs from "fs-extra"
 
 const rootDir = getRootDir()
 const createKKDir = path.join(rootDir, "../create-kunkun")
-// const extensionsDir = path.join(rootDir, "../extensions")
-// const extensionNames = fs.readdirSync(extensionsDir)
-// const extensionsDirs = extensionNames
-// 	.map((name) => path.join(extensionsDir, name))
-// 	.filter((p) => fs.existsSync(path.join(p, "package.json")))
 
 const createKKDistDir = path.join(createKKDir, "dist")
 const createKKIndexjsPath = path.join(createKKDistDir, "index.mjs")
@@ -25,7 +20,6 @@ fs.rmdirSync(testDir, { recursive: true })
 fs.mkdirpSync(testDir)
 
 const templateData: Record<string, { dir: string; buildResult: BuildResult }> = {}
-// const extensionData: Record<string, { dir: string; buildResult: BuildResult }> = {}
 
 await Promise.all(
 	templateNames.map(async (templateName) => {
@@ -39,17 +33,19 @@ await Promise.all(
 		}
 	})
 )
+console.log(templateData)
 
 test("Template Exist", () => {
 	Object.entries(templateData).forEach(async ([templateName, { dir }]) => {
-		expect(await Bun.file(dir).exists()).toBeTrue()
+		console.log("Expect dir exist: ", dir)
+		expect(fs.existsSync(dir)).toBeTrue()
 	})
 })
 
 test("Build Result Tarball Exist", () => {
 	Object.entries(templateData).forEach(async ([templateName, { buildResult, dir }]) => {
 		const expectedTarballPath = path.join(dir, buildResult.tarballFilename)
-		expect(await Bun.file(expectedTarballPath).exists()).toBeTrue()
+		expect(fs.existsSync(expectedTarballPath)).toBeTrue()
 	})
 })
 
