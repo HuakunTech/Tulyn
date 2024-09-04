3ddf39c0e1fad92bd127af93af2d348b6aae8d62
Huakun Shen <huakun.shen@huakunshen.com>
2024-09-04 14:34:11 -0400

    chore: loose commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ export default {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
 		"body-max-line-length": [0, "always", 500],
+		"subject-empty": [0, "always", ""],
+		"type-empty": [0, "always", ""]
 	}
 }
