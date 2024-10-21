25f258ad328b9d7067977421b2b4de4f8605f801
Huakun Shen <huakun.shen@huakunshen.com>
2024-10-20 23:21:15 -0400

    feat: add metadata label component

---
+++ apps/desktop/components/Metadata/label.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { IconType, List, ListSchema } from "@kksh/api/ui/worker"
+
+const props = defineProps<{
+	title: string
+	text?:
+		| string
+		| {
+				text: string
+				color: string
+		  }
+	icon?: {
+		type: IconType
+		value: string
+	}
+}>()
+</script>
+<template>
+	<div class="flex justify-between gap-1 py-1">
+		<span class="text-muted-foreground">{{ props.title }}</span>
+		<span class="">{{ props.text }}</span>
+	</div>
+</template>
